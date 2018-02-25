212573ba8a0566cb07b2d98b9872690b684b2d87
unknown <tadeup1@gmail.com>
2018-02-25 15:56:01 -0300

    melhorado o 2sls

--- tut2/tutorial 2.R
+++ tut2/tutorial 2.R
@@ -4,6 +4,7 @@ library(dplyr)
 
 setwd("C:\\Users\\tadeu\\Desktop\\FGV\\semestre 7\\econometria 3\\tutoriais\\tut2")
 df <- read.dta13("Problem2.dta")
+df <- df[df$hours != 0,]
 
 eq1 <- hours ~ lwage + educ + age + kidslt6 + kidsge6 + nwifeinc
 eq2 <- lwage ~ hours + educ + exper + expersq
@@ -22,3 +23,24 @@ systemfit(
   inst = instrument1,
   data = df) %>% 
   summary()
+
+#2sls na mão
+#jeito 1 ####
+#regride as var dependentes nas variaveis exogenas e achamos
+#uma combinação linear das variaveis exogenas que será um instrumento (Z)
+fit_1.1 <- lm(hours ~ age + kidslt6 + kidsge6 + nwifeinc, df) %>% fitted()
+fit_2.1 <- lm(lwage ~ exper + expersq, df) %>% fitted()
+
+fit_1.2 <- lm(fit_1.1 ~ lwage + educ, df)
+fit_2.2 <- lm(fit_2.1 ~ hours + educ, df)
+
+#jeito 2 agora vai####
+#http://www.rpubs.com/cyobero/simultaneous-models
+#regride cada uma das equações na sua forma reduzida
+#e depois regride as equações originais substituindo as var endogenas pelas suas versões estimada a cima
+hours_redu <- lm(hours ~ age + kidslt6 + kidsge6 + nwifeinc+ exper + expersq, df) %>% fitted()
+lwage_redu <- lm(lwage ~ age + kidslt6 + kidsge6 + nwifeinc+ exper + expersq, df) %>% fitted()
+
+fit_1.2 <- lm(hours ~ lwage_redu + educ + age + kidslt6 + kidsge6 + nwifeinc, df)
+fit_2.2 <- lm(lwage ~ hours_redu + educ + exper + expersq, df)
+
